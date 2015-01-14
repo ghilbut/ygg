d08260e160d4c9b1b7bbd34a453577ff6f77c882
ghilbut <ghilbut@gmail.com>
2015-01-15 00:40:08 +0900

    change cpp-netlib include path and insert skeleton code for REST-API call.

--- localbox_proxy/impl/localbox_impl.cpp
+++ localbox_proxy/impl/localbox_impl.cpp
@@ -15,34 +15,43 @@ LocalBox::Impl::Impl(const LocalBoxDesc &desc, codebase::HttpWebsocket &ws)
     , ws_(ws) {
     // nothing
 
-	/*
+
+	// TODO(ghilbut): get network address dynamically.
 	Json::Value root(Json::objectValue);
 	root["box-id"] = desc_.id();
-	root["address"] = "0.0.0.0";
+	root["address"] = "127.0.0.1";
 	Json::FastWriter writer;
 	const std::string json = writer.write(root);
 
-	printf("%s\n", json.c_str());
 
-	try {
-	boost::network::uri::uri uri("http://www.daum.net/api/reg/localbox/proxy/address");
-	boost::network::http::client::request request(uri);
-	//request << boost::network::header("Connection", "close");
+
+	boost::network::http::client::request request("http://127.0.0.1/api/reg/localbox/proxy/address");
+	//boost::network::uri::uri uri("http://127.0.0.1/api/reg/localbox/proxy/address");
+	//boost::network::http::client::request request(uri);
+	request << boost::network::header("Connection", "close");
 	//request << boost::network::header("Content-Type", "application/json");
-	//request << boost::network::header("Accepts", "application/json");
-	//request << boost::network::body(json);
+	request << boost::network::header("Accept", "application/json");
+	request << boost::network::body(json);
 	boost::network::http::client client;
 	//boost::network::http::client::response response = client.post(request);
 	boost::network::http::client::response response = client.post(request, json, std::string("application/json"));
-	
-		//std::string body = boost::network::http::body(response);
-		//printf("%s\n", body.c_str());
-		printf("%s\n", response.body().c_str());
+
+	try {
+		std::string body = boost::network::http::body(response);
+		printf("%s\n", body.c_str());
+		//printf("%s\n", response.body().c_str());
+
+		// TODO(ghilbut): check status code and error handling.
+		Json::Reader reader;
+		reader.parse(body, root, false);
+		if (!root.isObject()) {
+			// TODO(ghilbut): error handling
+		}
+		// TODO(ghilbut): check return values. realy necessary?
 	}
 	catch (std::exception& e) {
 		printf("%s\n", e.what());
 	}
-	*/
 }
 
 LocalBox::Impl* LocalBox::Impl::New(const std::string &json, codebase::HttpWebsocket &ws) {
