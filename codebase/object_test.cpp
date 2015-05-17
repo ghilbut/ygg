020b02c8d3134711743d436671b6bbb35f31278c
ghilbut <ghilbut@gmail.com>
2015-05-17 20:55:38 +0900

    change test condition of 'ObjectTest::test_object_ptr_validation_as_container_hash_key'.

--- codebase/object_test.cpp
+++ codebase/object_test.cpp
@@ -1,29 +1,24 @@
 #include <gmock/gmock.h>
 
 #include "object.h"
+#include "test/test_mock.h"
 #include <map>
 #include <set>
 #include <unordered_map>
 #include <unordered_set>
 
 
-using namespace codebase;
+namespace codebase {
 
-
-class Mock {
-public:
-    MOCK_CONST_METHOD0(constructed, void());
-    MOCK_CONST_METHOD0(destructed, void());
-};
 
 class TestObject : public Object<TestObject> {
 public:
-    static Ptr New(const Mock * mock = nullptr) {
+    static Ptr New(const test::LifeCycleMock * mock = nullptr) {
         return new TestObject(mock);
     }
 
 private:
-    TestObject(const Mock * mock) 
+    TestObject(const test::LifeCycleMock * mock) 
         : Object()
         , mock_(mock) {
 
@@ -39,7 +34,7 @@ private:
     }
 
 private:
-    const Mock * mock_;
+    const test::LifeCycleMock * mock_;
 public:
     Ptr ptr_;
 };
@@ -47,7 +42,7 @@ public:
 
 TEST(ObjectTest, test_construction_and_destruction) {
 
-    Mock mock;
+    test::LifeCycleMock mock;
     EXPECT_CALL(mock, constructed()).Times(1);
     EXPECT_CALL(mock, destructed()).Times(1);
 
@@ -77,7 +72,7 @@ TEST(ObjectTest, DISABLED_test_circular) {
 
 TEST(ObjectTest, test_object_ptr_validation_as_container_hash_key) {
 
-    Mock mock;
+    test::LifeCycleMock mock;
     EXPECT_CALL(mock, constructed()).Times(1);
     EXPECT_CALL(mock, destructed()).Times(1);
 
@@ -85,29 +80,74 @@ TEST(ObjectTest, test_object_ptr_validation_as_container_hash_key) {
     TestObject::Ptr obj1(obj0);
     TestObject::Ptr obj2(obj1);
 
-    std::set<TestObject::Ptr> set;
-    set.insert(obj0);
-    EXPECT_NE(set.end(), set.find(obj1));
-    EXPECT_NE(set.end(), set.find(obj2));
+    TestObject * obj = obj0.get();
 
-    std::unordered_set<TestObject::Ptr, TestObject::Hash> uset;
-    uset.insert(obj0);
-    EXPECT_NE(uset.end(), uset.find(obj1));
-    EXPECT_NE(uset.end(), uset.find(obj2));
+    {
+        std::set<TestObject::Ptr> set;
+        set.insert(obj0);
+        EXPECT_NE(set.end(), set.find(obj0));
+        EXPECT_NE(set.end(), set.find(obj1));
+        EXPECT_NE(set.end(), set.find(obj2));
+        EXPECT_NE(set.end(), set.find(obj));
+    }
 
-    std::map<TestObject::Ptr, int> map;
-    map[obj0] = 0;
-    map[obj1] = 1;
-    map[obj2] = 2;
-    EXPECT_EQ(2, map[obj0]);
-    EXPECT_EQ(2, map[obj1]);
-    EXPECT_EQ(2, map[obj2]);
+    {
+        std::unordered_set<TestObject::Ptr, TestObject::Hash> uset;
+        uset.insert(obj0);
+        EXPECT_NE(uset.end(), uset.find(obj0));
+        EXPECT_NE(uset.end(), uset.find(obj1));
+        EXPECT_NE(uset.end(), uset.find(obj2));
+        EXPECT_NE(uset.end(), uset.find(obj));
+    }
 
-    std::unordered_map<TestObject::Ptr, int, TestObject::Hash> umap;
-    umap[obj0] = 0;
-    umap[obj1] = 1;
-    umap[obj2] = 2;
-    EXPECT_EQ(2, umap[obj0]);
-    EXPECT_EQ(2, umap[obj1]);
-    EXPECT_EQ(2, umap[obj2]);
+    {
+        std::map<TestObject::Ptr, int> map;
+        map[obj0] = 0;
+        EXPECT_EQ(0, map[obj0]);
+        EXPECT_EQ(0, map[obj1]);
+        EXPECT_EQ(0, map[obj2]);
+        EXPECT_EQ(0, map[obj]);
+        map[obj1] = 1;
+        EXPECT_EQ(1, map[obj0]);
+        EXPECT_EQ(1, map[obj1]);
+        EXPECT_EQ(1, map[obj2]);
+        EXPECT_EQ(1, map[obj]);
+        map[obj2] = 2;
+        EXPECT_EQ(2, map[obj0]);
+        EXPECT_EQ(2, map[obj1]);
+        EXPECT_EQ(2, map[obj2]);
+        EXPECT_EQ(2, map[obj]);
+        map[obj] = 3;
+        EXPECT_EQ(3, map[obj0]);
+        EXPECT_EQ(3, map[obj1]);
+        EXPECT_EQ(3, map[obj2]);
+        EXPECT_EQ(3, map[obj]);
+    }
+
+    {
+        std::unordered_map<TestObject::Ptr, int, TestObject::Hash> umap;
+        umap[obj0] = 0;
+        EXPECT_EQ(0, umap[obj0]);
+        EXPECT_EQ(0, umap[obj1]);
+        EXPECT_EQ(0, umap[obj2]);
+        EXPECT_EQ(0, umap[obj]);
+        umap[obj1] = 1;
+        EXPECT_EQ(1, umap[obj0]);
+        EXPECT_EQ(1, umap[obj1]);
+        EXPECT_EQ(1, umap[obj2]);
+        EXPECT_EQ(1, umap[obj]);
+        umap[obj2] = 2;
+        EXPECT_EQ(2, umap[obj0]);
+        EXPECT_EQ(2, umap[obj1]);
+        EXPECT_EQ(2, umap[obj2]);
+        EXPECT_EQ(2, umap[obj]);
+        umap[obj] = 3;
+        EXPECT_EQ(3, umap[obj0]);
+        EXPECT_EQ(3, umap[obj1]);
+        EXPECT_EQ(3, umap[obj2]);
+        EXPECT_EQ(3, umap[obj]);
+    }
 }
+
+
+}  // namespace codebase
