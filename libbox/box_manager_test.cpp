cecbdaa108277faab0eeddb704e6db4bc25da4c6
ghilbut <ghilbut@gmail.com>
2015-06-28 22:09:03 +0900

    rename variables of libbox/box_manager_test.cpp file.

--- libbox/box_manager_test.cpp
+++ libbox/box_manager_test.cpp
@@ -72,33 +72,33 @@ TEST_F(BoxManagerTest, test_bind_user_and_box) {
     static const std::string user_text("user-text");
 
     // box connection and conn
-    Connection::Ptr box(FakeConnection::New());
-    Connection::Ptr box_conn(((FakeConnection*) box.get())->conn());
+    Connection::Ptr box_client(FakeConnection::New());
+    Connection::Ptr box_server(((FakeConnection*) box_client.get())->conn());
 
     ConnectionDelegateMock box_mock;
-    EXPECT_CALL(box_mock, OnText(box.get(), user_text));
-    EXPECT_CALL(box_mock, OnClosed(box.get()));
-    box->BindDelegate(&box_mock);
+    EXPECT_CALL(box_mock, OnText(box_client.get(), user_text));
+    EXPECT_CALL(box_mock, OnClosed(box_client.get()));
+    box_client->BindDelegate(&box_mock);
 
     // user connection and conn
-    Connection::Ptr user(FakeConnection::New());
-    Connection::Ptr user_conn(((FakeConnection*) user.get())->conn());
+    Connection::Ptr user_client(FakeConnection::New());
+    Connection::Ptr user_server(((FakeConnection*) user_client.get())->conn());
 
     ConnectionDelegateMock user_mock;
-    EXPECT_CALL(user_mock, OnText(user.get(), box_text));
-    EXPECT_CALL(user_mock, OnClosed(user.get()));
-    user->BindDelegate(&user_mock);
+    EXPECT_CALL(user_mock, OnText(user_client.get(), box_text));
+    EXPECT_CALL(user_mock, OnClosed(user_client.get()));
+    user_client->BindDelegate(&user_mock);
 
     // bind each other by manager
     BoxManager manager;
-    manager.BindBoxConnection(box_conn);
-    manager.BindUserConnection(user_conn);
+    manager.BindBoxConnection(box_server);
+    manager.BindUserConnection(user_server);
 
-    box->SendText(valid_box_json());
-    user->SendText(valid_user_json());
+    box_client->SendText(valid_box_json());
+    user_client->SendText(valid_user_json());
 
-    box->SendText(box_text);
-    user->SendText(user_text);
+    box_client->SendText(box_text);
+    user_client->SendText(user_text);
 }
 
 
