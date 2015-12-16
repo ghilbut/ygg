30a16552978ba6558504e435e752d9b3434e58aa
ghilbut <ghilbut@gmail.com>
2015-12-16 22:38:30 +0900

    complete BypassAdapter class and test.

--- libtarget/server/bypass_adapter_test.cpp
+++ libtarget/server/bypass_adapter_test.cpp
@@ -6,7 +6,9 @@
 #include "net/local_connection.h"
 #include "test/mock.h"
 #include "test/vars.h"
+#include <vector>
 #include <string>
+#include <unordered_map>
 
 
 namespace ygg {
@@ -18,17 +20,30 @@ class BypassAdapterTest : public ::testing::Test {
 public:
     void SetUp() {
 
-        ctrl_conn_ = LocalConnection::New();
-        auto ctrl_conn_tmp = (LocalConnection*) ctrl_conn_.get();
-        auto ctrl_proxy_conn(ctrl_conn_tmp->other());
-        const auto ctrl_desc_json(test::GetCtrlJson("A", "B"));
-        const auto ctrl_desc(CtrlDesc::New(ctrl_desc_json));
-        ctrl_proxy_ = CtrlProxy::New(ctrl_proxy_conn, ctrl_desc);
+        std::vector<std::string> users;
+        users.push_back("A0");
+        users.push_back("A1");
+        users.push_back("A2");
+
+        const std::string endpoint("B");
+
+        for (const auto user : users) {
+
+            auto ctrl_conn(LocalConnection::New());
+            ctrl_conns_.push_back(ctrl_conn);
+
+            auto ctrl_conn_tmp = (LocalConnection*) ctrl_conn.get();
+            auto ctrl_proxy_conn(ctrl_conn_tmp->other());
+            const auto ctrl_desc_json(test::GetCtrlJson(user, endpoint));
+            const auto ctrl_desc(CtrlDesc::New(ctrl_desc_json));
+            auto ctrl_proxy(CtrlProxy::New(ctrl_proxy_conn, ctrl_desc));
+            ctrl_proxys_.push_back(ctrl_proxy);
+        }
 
         target_conn_ = LocalConnection::New();
         auto target_conn_tmp = (LocalConnection*)target_conn_.get();
         auto target_proxy_conn(target_conn_tmp->other());
-        const auto target_desc_json(test::GetTargetJson("B"));
+        const auto target_desc_json(test::GetTargetJson(endpoint));
         const auto target_desc(TargetDesc::New(target_desc_json));
         target_proxy_ = TargetProxy::New(target_proxy_conn, target_desc);
     }
@@ -36,13 +51,13 @@ public:
     void TearDown() {
         target_proxy_ = nullptr;
         target_conn_ = nullptr;
-        ctrl_proxy_ = nullptr;
-        ctrl_conn_ = nullptr;
+        ctrl_proxys_.clear();
+        ctrl_conns_.clear();
     }
 
 protected:
-    Connection::Ptr ctrl_conn_;
-    CtrlProxy::Ptr ctrl_proxy_;
+    std::vector<Connection::Ptr> ctrl_conns_;
+    std::vector<CtrlProxy::Ptr> ctrl_proxys_;
     Connection::Ptr target_conn_;
     TargetProxy::Ptr target_proxy_;
 };
@@ -51,43 +66,57 @@ protected:
 TEST_F(BypassAdapterTest, ctrl_send_text_to_target) {
 
     auto adapter(BypassAdapter::New(target_proxy_));
-    adapter->SetCtrl(ctrl_proxy_);
+    for (auto & ctrl : ctrl_proxys_) {
+        adapter->SetCtrl(ctrl);
+    }
 
     const auto expected(test::GetRandomString());
+    const int n = ctrl_proxys_.size();
 
     test::ConnectionDelegateMock mock;
-    EXPECT_CALL(mock, OnText(target_conn_.get(), expected));
+    EXPECT_CALL(mock, OnText(target_conn_.get(), expected)).Times(n);
     target_conn_->BindDelegate(&mock);
 
-    ctrl_conn_->SendText(expected);
+    for (auto & ctrl : ctrl_conns_) {
+        ctrl->SendText(expected);
+    }
 }
 
 
 TEST_F(BypassAdapterTest, ctrl_send_bytes_to_target) {
 
     auto adapter(BypassAdapter::New(target_proxy_));
-    adapter->SetCtrl(ctrl_proxy_);
+    for (auto & ctrl : ctrl_proxys_) {
+        adapter->SetCtrl(ctrl);
+    }
 
     const auto expected(test::GetRandomBytes());
+    const int n = ctrl_proxys_.size();
 
     test::ConnectionDelegateMock mock;
-    EXPECT_CALL(mock, OnBinary(target_conn_.get(), expected));
+    EXPECT_CALL(mock, OnBinary(target_conn_.get(), expected)).Times(n);
     target_conn_->BindDelegate(&mock);
 
-    ctrl_conn_->SendBinary(expected);
+    for (auto & ctrl : ctrl_conns_) {
+        ctrl->SendBinary(expected);
+    }
 }
 
 
 TEST_F(BypassAdapterTest, target_send_text_to_ctrl) {
 
     auto adapter(BypassAdapter::New(target_proxy_));
-    adapter->SetCtrl(ctrl_proxy_);
+    for (auto & ctrl : ctrl_proxys_) {
+        adapter->SetCtrl(ctrl);
+    }
 
     const auto expected(test::GetRandomString());
 
     test::ConnectionDelegateMock mock;
-    EXPECT_CALL(mock, OnText(ctrl_conn_.get(), expected));
-    ctrl_conn_->BindDelegate(&mock);
+    for (auto & ctrl : ctrl_conns_) {
+        EXPECT_CALL(mock, OnText(ctrl.get(), expected));
+        ctrl->BindDelegate(&mock);
+    }
 
     target_conn_->SendText(expected);
 }
@@ -96,13 +125,17 @@ TEST_F(BypassAdapterTest, target_send_text_to_ctrl) {
 TEST_F(BypassAdapterTest, target_send_bytes_to_ctrl) {
 
     auto adapter(BypassAdapter::New(target_proxy_));
-    adapter->SetCtrl(ctrl_proxy_);
+    for (auto & ctrl : ctrl_proxys_) {
+        adapter->SetCtrl(ctrl);
+    }
 
     const auto expected(test::GetRandomBytes());
 
     test::ConnectionDelegateMock mock;
-    EXPECT_CALL(mock, OnBinary(ctrl_conn_.get(), expected));
-    ctrl_conn_->BindDelegate(&mock);
+    for (auto & ctrl : ctrl_conns_) {
+        EXPECT_CALL(mock, OnBinary(ctrl.get(), expected));
+        ctrl->BindDelegate(&mock);
+    }
 
     target_conn_->SendBinary(expected);
 }
